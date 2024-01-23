717c64ae387ef1ec9425ec1ca868f7e738c64f83
George Campbell <campbell.goe@gmail.com>
2024-01-23 20:08:14 +0000

    hovered state

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import "./tailwind.output.css";
 
 import './styles.css'
 
-const Scene = ({ color = 0x00ff00, vector = [0,0,0] } = {}) => {
+const Scene = ({ color = 0x00ff00, vector = [0, 0, 0] } = {}) => {
   const boxMeshRef = useRef()
   const [newColor, setNewColor] = useState(color);
   useEffect(() => {
@@ -16,13 +16,23 @@ const Scene = ({ color = 0x00ff00, vector = [0,0,0] } = {}) => {
       boxMeshRef.current.position.x += vector[0]
       boxMeshRef.current.position.y += vector[1]
       boxMeshRef.current.position.z += vector[2]
+      // boxMeshRef.current.
     }
   }, [vector])
+  const [hovered, setHover] = useState(false)
+
   return (
-    <mesh ref={boxMeshRef}>
-      <boxGeometry />
-      <meshBasicMaterial color={newColor} wireframe />
-    </mesh>
+    <>
+      <mesh ref={boxMeshRef} scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}>
+        <boxGeometry />
+        <meshStandardMaterial color={newColor} emissive={0xffffff} emissiveIntensity={hovered ? 0.1 : 0} />
+      </mesh>
+      <ambientLight intensity={0.1} />
+      <pointLight position={[0, 1, 1]} />
+
+    </>
   )
 }
 
@@ -49,13 +59,13 @@ const App = () => {
         KeyQ: [0, 1, 0],
         KeyE: [0, -1, 0],
       }
-      if(code in codeMap){
+      if (code in codeMap) {
         const vector = codeMap[code]
         setVector(vector)
       } else {
         setVector([0, 0, 0])
       }
-      
+
     }
     document.addEventListener('keydown', onDocumentKey)
     return () => {
@@ -67,7 +77,7 @@ const App = () => {
     rerender()
   }} />
     <Canvas camera={{ position: [0, 0, 2] }}>
-      <Scene color={color.current} vector={vector}/>
+      <Scene color={color.current} vector={vector} />
     </Canvas>
   </>
 }
